a1ab5f8d36861a24716138e51b59ba26694a5c3d
Vitaliy <theyamshikov@yandex.ru>
2024-03-24 22:18:42 +0500

    upd

--- src/resources/config/commands.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Untek\Framework\Console\Symfony4\Interfaces\CommandConfiguratorInterface;
-
-\Untek\Core\Code\Helpers\DeprecateHelper::hardThrow();
-return function (CommandConfiguratorInterface $commandConfigurator) {
-    $commandConfigurator->registerCommandClass(\Untek\Utility\Init\Presentation\Cli\Commands\InitCommand::class);
-};
